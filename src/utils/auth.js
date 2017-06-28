dba85f3121d698a423736734de2f1ad7d76a1e3b
fireyy <fireyy@gmail.com>
2017-06-28 10:37:23 +0800

    fix: user auth high order component

---
+++ src/utils/auth.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+const validate = function (history) {
+  const isLoggedIn = !!window.localStorage.getItem('uid')
+  console.log(history.location.pathname)
+  if (!isLoggedIn && history.location.pathname != '/login') {
+    history.replace('/login')
+  }
+}
+
+/**
+ * Higher-order component (HOC) to wrap restricted pages
+ */
+export default function authHOC(BaseComponent) {
+    class Restricted extends Component {
+        componentWillMount() {
+            this.checkAuthentication(this.props);
+        }
+        componentWillReceiveProps(nextProps) {
+            if (nextProps.location !== this.props.location) {
+                this.checkAuthentication(nextProps);
+            }
+        }
+        checkAuthentication(params) {
+            const { history } = params;
+            validate(history);
+        }
+        render() {
+            return <BaseComponent {...this.props} />;
+        }
+    }
+    return withRouter(Restricted);
+}
