9cc762fa4de1e3df329025c92f5b303de1544528
adolphlwq <kenan3015@gmail.com>
2017-06-30 17:12:10 +0800

    add confirm password and redirect to /login when register successfully

--- src/views/Register/index.jsx
+++ src/views/Register/index.jsx
@@ -31,16 +31,22 @@ class Register extends React.Component {
       loading: true
     });
     const data = this.props.form.getFieldsValue()
-    if (data.user ===  undefined || data.password === undefined) {
+    if (data.user ===  undefined || data.password === undefined || data.confirmpassword === undefined) {
       this.setState({
         loading: false
       });
-      message.error("Incorrect user or password");
-    } else {
+      message.error("Incorrect user or password or confirmpassword");
+    } else if (data.password !== data.confirmpassword) {
       this.setState({
         loading: false
       });
-      message.success("Welcome " + data.user)
+      message.error("please input correct password and confirmpassword");
+    }else {
+      this.setState({
+        loading: false
+      });
+      this.props.history.replace('/login');
+      message.success("Welcome " + data.user + " please login.")
     }
   }
   
@@ -57,12 +63,17 @@ class Register extends React.Component {
             <h2 className="logo"><span>logo</span></h2>
             <FormItem>
               {getFieldDecorator('user')(
-                <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder='admin' />
+                <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder='please input name' />
               )}
             </FormItem>
             <FormItem>
               {getFieldDecorator('password')(
-                <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type='password' placeholder='123456' />
+                <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type='password' placeholder='please input password' />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('confirmpassword')(
+                <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type='password' placeholder='please input confirm password' />
               )}
             </FormItem>
             <p>
